8e2513d25dfbaf51f785978672fd807ff523bd4d
Vatsala Vats <vatsalavats18@gmail.com>
2021-06-28 01:18:04 +0530

    Array reversal added

---
+++ ArrayReverse.c
@@ -0,0 +1,24 @@
+//REVERSE OF ARRAY
+
+#include<stdio.h>
+void main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int first=0,last=n-1,temp;
+    while(first<=last){
+        temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+
+        first++;
+        last--;
+    }
+    printf("\nReversed Array is : ");
+    for(int i=0;i<n;i++){
+        printf("\t%d",arr[i]);
+    }
+}
