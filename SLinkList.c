96e4f1bf85323a3f08981829114a5f0537d4506b
Vatsala Vats <vatsalavats18@gmail.com>
2021-07-14 12:50:21 +0530

    Modified

--- SLinkList.c
+++ SLinkList.c
@@ -45,6 +45,7 @@ void beg_insert(){
 
         printf("\nNode inserted at beginning...");
     }
+    return fnode;
 }
 void loc_insert(){
     struct node *prev,*temp;
@@ -62,17 +63,18 @@ void loc_insert(){
 
         prev = fnode;
         
-        for(int i=0;i<loc;i++){
+        for(int i=1;i<loc;i++){
             
             prev = prev->next;
 
             if(prev == NULL){
             printf("\nCan't insert...");
             }
-
+        }
+        
             temp->next = prev->next;
             prev->next = temp; 
-        }
+            printf("\nNode inserted...");
     }
 }
 void end_insert(){
@@ -119,7 +121,59 @@ void beg_delete(){
     }
 }
 void loc_delete(){
-    printf("");
+    int loc;
+    struct node *temp,*prev;
+    if(fnode == NULL){
+        printf("\nUnderflow...");
+    }
+    else{
+        printf("\nEnter location after which you want to delete node :-");
+        scanf("%d",&loc);
+        temp = fnode;
+        for(int i=0;i<loc;i++){
+            prev = temp;
+            temp = temp->next;
+            if(temp == NULL){
+                printf("\nThere are less than %d elements in list...",loc);
+                return;
+            }
+        }
+        prev->next = temp->next;
+        free(temp);
+        printf("\nDeleted node%d.",loc+1);
+    }
+    
+}
+void num_delete(){
+     struct node *temp,*prev;
+     int num;
+    if(fnode == NULL){
+        printf("\nUnderflow...");
+    }
+    else{
+        printf("Enter num you want to delete : ");
+        scanf("%d",&num);
+            temp = fnode;
+        if(fnode->data == num){
+            temp = fnode;
+            fnode = temp->next;
+            free(temp);
+            printf("\nNode Deleted...");
+        }
+        else{
+            
+            while(temp->next != NULL && temp->data != num) {
+                prev = temp;
+                temp = temp->next;
+            }        
+
+            if(temp->data == num) {
+                 prev->next = prev->next->next;
+                free(temp);  
+             }
+             printf("\nNode Deleted...");
+         }
+    }
 }
 void end_delete(){
     struct node *temp,*prev;
@@ -146,7 +200,7 @@ void end_delete(){
 void display(){
     struct node *temp;
     if(fnode == NULL){
-        printf("List is empty...");
+        printf("\nList is empty...");
     }
     else{
         temp = fnode;
@@ -157,12 +211,28 @@ void display(){
     }
 }
 
+void reverse(){
+    struct node *next = NULL,*prev = NULL,*current = fnode;
+    if(fnode == NULL){
+        printf("\nUnderflow...");
+    }
+    else{
+       while(current != NULL){
+           next = current->next;
+           current->next = prev;
+
+           prev = current;
+           current = next;
+       }
+        fnode = prev;
+    }
+}
 int main(){
     int choice;
     int ch1,ch2;
     do{
     printf("\nEnter your choice :-\n");
-    printf("0 : EXIT\n1 : CREATE LIST\n2 : INSERT\n3 : DELETE\n4 : SEARCH\n5 : DISPLAY\n");
+    printf("\n1 : CREATE LIST\n2 : INSERT\n3 : DELETE\n4 : SEARCH\n5 : REVERSE\n6 : DISPLAY\n0 : EXIT\n");
     scanf("%d",&choice);
     switch(choice){
         case 1 : 
@@ -183,7 +253,7 @@ int main(){
                 break;
         case 3 : 
                 printf("\nSelect one : ");
-                printf("1 : FROM BEGINNING\n2 : FROM ANY LOCATION\n3 : FROM END\n");
+                printf("\n1 : FROM BEGINNING\n2 : FROM ANY LOCATION\n3 : FROM END\n4 : ANY ELEMENT\n");
                 scanf("%d",&ch2);
                 switch(ch2){
                     case 1 : beg_delete();
@@ -192,13 +262,19 @@ int main(){
                              break;
                     case 3 : end_delete();
                              break;
+                    case 4 : num_delete();
+                             break;
                 }
                 break;
         case 4 : 
                 break;
-        case 5 : display();
+        case 5 : reverse();
+                break;
+        case 6 : display();
                 break;
         case 0 : exit(0);
+
+        default : printf("Invalid input...");
 
     }
     }while(choice != 0);
